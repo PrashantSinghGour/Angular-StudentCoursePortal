0bd27621faab362a85dc74e65646b8ded95ca7b7
Prashant Singh Gour <47054841+PrashantSinghGour@users.noreply.github.com>
2019-03-16 21:19:42 +0530

    values removed

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,12 +1,12 @@
 export const environment = {
   production: true,
   firebaseConfig : {
-    apiKey: "AIzaSyA4JA9HPB3IVg9Qx1IZER8ehAfN2b-GuyQ",
-    authDomain: "mcurd-6a4ae.firebaseapp.com",
-    databaseURL: "https://mcurd-6a4ae.firebaseio.com",
-    projectId: "mcurd-6a4ae",
-    storageBucket: "mcurd-6a4ae.appspot.com",
-    messagingSenderId: "89804390813"
+    apiKey: "//Your Value",
+    authDomain: "//Your Value",
+    databaseURL: "//Your Value",
+    projectId: "//Your Value",
+    storageBucket: "//Your Value",
+    messagingSenderId: "//Your Value"
     
   }
 
